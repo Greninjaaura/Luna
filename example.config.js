60d76627361789c9cb366d655cff1dfdb285015b
Greninjaaura <145592206+Greninjaaura@users.noreply.github.com>
2024-01-16 15:53:20 +0530

    Create example.config.js

---
+++ example.config.js
@@ -0,0 +1,30 @@
+exports.server = 'sim.smogon.com';
+exports.port = 8000;
+exports.serverid = 'showdown';
+exports.nick = 'Universal code';
+exports.pass = '';
+exports.rooms = [''];
+exports.privaterooms = [''];
+exports.commandcharacter = exports.commandCharacter = '.';
+exports.defaultrank = '+';
+exports.groups = {
+    ' ': 0,
+    '+': 1,
+    '%': 2,
+    '@': 3,
+    '#': 4,
+    '&': 5,
+    '~': 6
+};
+exports.watchconfig = false;
+exports.secprotocols = [];
+exports.debuglevel = 0;
+exports.allowmute = true;
+exports.punishvals = {
+    1: 'warn',
+    2: 'mute',
+    3: 'hourmute',
+    4: 'roomban',
+    5: 'ban'
+};
+exports.googleapikey = '';
